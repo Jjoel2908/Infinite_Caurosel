b10251f0c3875fffef692e0e3c60721d8da022ad
Joel Alvarado <joelalvaradom02@gmail.com>
2024-02-28 10:42:43 -0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Header = () => {
+	return (
+		<div className='bg-gradient-to-r from-cyan-600 to-blue-700 text-slate-50'>
+			<div className='px-10 justify-center py-4'>
+				<a href='#'>
+					<h1 className='text-xl text-center'>Carrusel Infinito</h1>
+				</a>
+			</div>
+		</div>
+	);
+};
